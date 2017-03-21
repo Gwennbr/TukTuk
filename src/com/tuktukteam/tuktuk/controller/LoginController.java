c121d67c73b51a4a8bdd9e04eb3258ac8985fec2
Gwenn <Administrateur@m2i-34732-9>
2017-03-21 14:57:02 +0100

    Signed-off-by: Gwenn <Administrateur@m2i-34732-9>

--- src/com/tuktukteam/tuktuk/controller/LoginController.java
+++ src/com/tuktukteam/tuktuk/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController
 			e.printStackTrace();
 		} 
 		
-		System.out.println(c.getNuméroCarteBancaire() + " " + c.getPictogramme());
+		System.out.println(c.getNumeroCarteBancaire() + " " + c.getPictogramme());
 		
 		return "login"; 
 	}
