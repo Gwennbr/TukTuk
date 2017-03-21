a206ee33b56f98123d10725e3b4e89dd68443e64
Gwenn <Administrateur@m2i-34732-9>
2017-03-21 13:46:40 +0100

    Change-Id: I6b96c0573ae88ff0640702bdf7c7cf465608b718 Signed-off-by: Gwenn <Administrateur@m2i-34732-9>

--- src/com/tuktukteam/tuktuk/controller/LoginController.java
+++ src/com/tuktukteam/tuktuk/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.tuktukteam.tuktuk.controller;
 
+import java.io.IOException;
+import java.net.URL;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tuktukteam.tuktuk.model.Client;
 
 @Controller
 public class LoginController
@@ -10,6 +16,19 @@ public class LoginController
 	@RequestMapping("/login")
 	public String login()
 	{
+		ObjectMapper mapper = new ObjectMapper();
+		
+		Client c = null;
+		try
+		{
+			Client c = mapper.readValue(new URL("http://localhost/api/toto"), Client.class);
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return "login";
 	}
 }
---
+++ src/com/tuktukteam/tuktuk/restcontroller/TestsREST.java
@@ -0,0 +1,26 @@
+package com.tuktukteam.tuktuk.restcontroller;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tuktukteam.tuktuk.model.Client;
+
+@RestController
+public class TestsREST
+{
+	@RequestMapping(value = "/toto", method = RequestMethod.GET)
+	public ResponseEntity<Client> toto()
+	{
+		Client c = new Client();
+		c.setDateValiditeCB(new Date());
+		c.setNuméroCarteBancaire(65498765);
+		c.setPictogramme(123);
+		
+		return new ResponseEntity<>(c, HttpStatus.OK);
+	}
+}
