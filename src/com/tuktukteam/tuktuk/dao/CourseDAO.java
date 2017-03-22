ff7e05f9044478a7c99686a1de5c541c59f5b39d
Rémi <Administrateur@M2i-34732-8>
2017-03-22 09:55:10 +0100

    Ajout annotation @Transactionnal et @Repository
    Signed-off-by: Rémi <Administrateur@M2i-34732-8>

--- src/com/tuktukteam/tuktuk/dao/CourseDAO.java
+++ src/com/tuktukteam/tuktuk/dao/CourseDAO.java
@@ -1,8 +1,13 @@
 package com.tuktukteam.tuktuk.dao;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.tuktukteam.genericdao.GenericDAO;
 import com.tuktukteam.tuktuk.model.Course;
 
+@Transactional
+@Repository
 public class CourseDAO extends GenericDAO<Course, Integer> {
 
 	public CourseDAO() {
