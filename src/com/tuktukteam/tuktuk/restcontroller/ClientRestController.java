3fb28f69e9d2dacc7bd63c934fd908ef6e01e229
Rémi <Administrateur@M2i-34732-8>
2017-03-23 10:08:19 +0100

    Vérification client ou conducteur en session
    Signed-off-by: Rémi <Administrateur@M2i-34732-8>

--- src/com/tuktukteam/tuktuk/restcontroller/ClientRestController.java
+++ src/com/tuktukteam/tuktuk/restcontroller/ClientRestController.java
@@ -15,44 +15,60 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tuktukteam.tuktuk.dao.ClientDAO;
 import com.tuktukteam.tuktuk.model.Client;
+import com.tuktukteam.tuktuk.model.Conducteur;
 import com.tuktukteam.tuktuk.model.Course;
 
 @RestController
 @RequestMapping("/client")
 public class ClientRestController {
-	
+
 	@Autowired
 	private ClientDAO clientDAO;
-	
-	@RequestMapping(value="/profil", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/profil", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<Client> getProfile(HttpSession session) {
+
+		Client c = (Client) session.getAttribute("client");
+
+		if (c != null) {
+			int idC = c.getId();
+			return new ResponseEntity<Client>(this.clientDAO.find(idC),	HttpStatus.OK);
+		}
 		
-		return new ResponseEntity<Client>(this.clientDAO.find(((Client) session.getAttribute("user")).getId()), HttpStatus.OK);
+		return new ResponseEntity<Client>(HttpStatus.FORBIDDEN);
 	}
-	
-	@RequestMapping(value="/{id}/courses", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}/courses", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<List<Course>> getCourses(@PathVariable int id) {		
-		//TODO TOUT DOUX : si erreur, gérer list course null
-		return new ResponseEntity<List<Course>>(this.clientDAO.find(id).getCourses(), HttpStatus.OK);
+	public ResponseEntity<List<Course>> getCourses(@PathVariable int id, HttpSession session) {
+		// TODO TOUT DOUX : si erreur, gérer list course null
+		Client client = (Client) session.getAttribute("client");
+		Conducteur conducteur = (Conducteur) session.getAttribute("conducteur");
+
+		if (conducteur != null || (client != null && client.getId() == id)) {
+			return new ResponseEntity<List<Course>>(this.clientDAO.find(id).getCourses(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<List<Course>>(HttpStatus.FORBIDDEN);
+		}
 	}
-	
-//	@RequestMapping(value="/{id}/commentaire/{idCourse}", method = RequestMethod.PUT)
-//	@ResponseBody
-//	public ResponseEntity<Course> setCommAndNote(@PathVariable int id, @PathVariable int idCourse, @RequestParam String commentaire, @RequestParam float note ) {
-//				
-//		return null;
-//	}
-	
-	@RequestMapping(value="/{id}/infos", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}/infos", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<Client> getInfos(@PathVariable int id) {
-		Client c = clientDAO.find(id);
-		c.setDateValiditeCB(null);
-		c.setNumeroCarteBancaire(null);
-		c.setPictogramme(null);		
-		c.setMail(null);
-		return new ResponseEntity<Client>(c, HttpStatus.OK);
-	}	
+	public ResponseEntity<Client> getInfos(@PathVariable int id, HttpSession session) {
+
+		Client client = (Client) session.getAttribute("client");
+		Conducteur conducteur = (Conducteur) session.getAttribute("conducteur");
+
+		if (conducteur != null) {
+			Client c = clientDAO.find(id);
+			c.setDateValiditeCB(null);
+			c.setNumeroCarteBancaire(null);
+			c.setPictogramme(null);
+			c.setMail(null);
+			return new ResponseEntity<Client>(c, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Client>(HttpStatus.FORBIDDEN);
+		}
+	}
 }
