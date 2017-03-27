bbff965972ca3da6099a04105caa68c9aa932ac9
Rémi <Administrateur@M2i-34732-8>
2017-03-27 11:42:18 +0200

    Correction Restriction d'accés aus Rest Controllers
    Signed-off-by: Rémi <Administrateur@M2i-34732-8>

--- src/com/tuktukteam/autosecurity/AccessTokenSecurity.java
+++ src/com/tuktukteam/autosecurity/AccessTokenSecurity.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.tuktukteam.tools.Random;
-import com.tuktukteam.tuktuk.model.Client;
 
 import lombok.Getter;
 import lombok.Setter;
