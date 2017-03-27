e2c633e6e3f55cfd410f39205a2be29d65dae550
Administrateur <Administrateur@M2i-34732-3>
2017-03-27 13:49:21 +0200

    ajout marqueur map

--- WebContent/resources/js/gmap.js
+++ WebContent/resources/js/gmap.js
@@ -1,28 +1,27 @@
-var app = angular.module('myApp', ['ngMap']);
-
-  app.controller('mapController', function($interval) {
-    var vm = this;
-    vm.positions = [];
-	
-    var generateMarkers = function() {
-      
-    };
-
-    $interval(generateMarkers, 2000);
-  });
-  
-  app.controller('MyCtrl', function(NgMap) {
-  var vm = this;
-  vm.types = "['establishment']";
-  vm.placeChanged = function() {
-    vm.place = this.getPlace();
-    console.log('location', vm.place.geometry.location);
-    vm.map.setCenter(vm.place.geometry.location);
-  }
-  NgMap.getMap().then(function(map) {
-    vm.map = map;
-  });
-  
+//var app = angular.module('myApp', ['ngMap']);
+//
+//  app.controller('mapController', function($interval) {
+//    var vm = this;
+//    vm.positions = [];
+//	
+//    var generateMarkers = function() {
+//      
+//    };
+//
+//    $interval(generateMarkers, 2000);
+//  });
+//  
+//  app.controller('MyCtrl', function(NgMap) {
+//  var vm = this;
+//  vm.types = "['establishment']";
+//  vm.placeChanged = function() {
+//    vm.place = this.getPlace();
+//    vm.map.setCenter(vm.place.geometry.location);
+//  }
+//  NgMap.getMap().then(function(map) {
+//    vm.map = map;
+//  });
+//  
   
   
 //  vm.addMarker = function(event) {
@@ -34,3 +33,5 @@ var app = angular.module('myApp', ['ngMap']);
 //    };
   
  });
+  
+  
