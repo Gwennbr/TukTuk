076657e2334383045c2b68b979b374f78d231f69
Gwenn <Administrateur@m2i-34732-9>
2017-03-28 09:04:24 +0200

    [MAJ] config adr serveur SQL
    Signed-off-by: Gwenn <Administrateur@m2i-34732-9>

--- src/com/tuktukteam/autosecurity/ResponseWrapper.java
+++ src/com/tuktukteam/autosecurity/ResponseWrapper.java
@@ -56,6 +56,8 @@ public class ResponseWrapper extends HttpServletResponseWrapper
 		super.setStatus(sc);
  		if (!bHeaderAdded && sc >= 200 && sc <= 299)
  		{
+ 			String token = (String) request.getAttribute(AccessTokenSecurity.TOKEN_HEADER_NAME);
+ 			
  			System.out.println("set status");
  			//super.addHeader(headerName, headerValue);
  			bHeaderAdded = true;
