edabf0dd4fad90873c7787febfa99cfacad97889
Administrateur <Administrateur@M2i-34732-3>
2017-03-28 09:24:26 +0200

    gmap

--- WebContent/resources/js/gmap.js
+++ WebContent/resources/js/gmap.js
@@ -28,6 +28,7 @@ var app = angular.module('myApp', ['ngMap']);
 
     $interval(generateMarkers, 5000);//refresh position
   });
+  
 //var app = angular.module('myApp', ['ngMap']);
 //
 //  app.controller('mapController', function($interval) {
