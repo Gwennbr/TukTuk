7e78bcfc2249e3066a88680817bc419de7231a47
Gwenn <Administrateur@m2i-34732-9>
2017-04-03 08:58:12 +0200

    depart
    Signed-off-by: Gwenn <Administrateur@m2i-34732-9>

---
+++ WebContent/resources/js/accueil.js
@@ -0,0 +1,35 @@
+
+//	rest.driver_GetRunsHistory(function(course) {
+//			course.forEach(function(item, index) {	    
+//			    var time = new Date((item.dateFinCourse - item.dateDebutCourse) - 3600000);
+//			    $("#history")
+//				.append(
+//						'<tr class="success">'
+//								+ '<td>' + item.adresseDepart + '</td>'
+//								+ '<td>' + time.getHours().toString().replace(/^(\d)$/,'0$1') + 'h' + time.getMinutes().toString().replace(/^(\d)$/,'0$1') + '</td>'
+//								+ '<td>' + item.prix + ' €</td>'
+//								+ '</tr>');
+//			});
+//	});
+//	
+//
+//rest.driver_GetMyProfil(function(profil) {
+//	$('#idClientNomPrenom').html()
+//	if (profil.available == true) {
+//		$('#toggle-trigger').bootstrapToggle('on')
+//	} else {
+//		$('#toggle-trigger').bootstrapToggle('off')
+//	}
+//});
+
+//function historyAppend(item, index) {
+//	var time = new Date((item.dateFinCourse - item.dateDebutCourse) - 3600000);
+//	$("#history").append(
+//			'<tr class="success">' + '<td>' + item.adresseDepart + '</td>'
+//					+ '<td>'
+//					+ time.getHours().toString().replace(/^(\d)$/, '0$1') + 'h'
+//					+ time.getMinutes().toString().replace(/^(\d)$/, '0$1')
+//					+ '</td>' + '<td>' + item.prix + ' €</td>' + '</tr>');
+//};
+
+
